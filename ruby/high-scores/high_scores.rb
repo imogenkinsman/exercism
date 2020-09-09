5c82ec28f7ee68181508311c338520de93ffce18
Imogen Kinsman <jade@thezets.com>
2020-09-09 12:51:22 -0700

    high scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,23 @@
+class HighScores
+  attr_reader :scores
+
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def latest_is_personal_best?
+    self.latest == self.personal_best
+  end
+
+  def personal_top_three
+    @scores.sort.reverse.first(3)
+  end
+end
