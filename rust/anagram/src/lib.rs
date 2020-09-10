f13d01a7a6b79c8db9b0449d2ecd036ee5cac232
Imogen Kinsman <jade@thezets.com>
2020-09-10 13:24:06 -0700

    anagram: refactor

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,36 +1,34 @@
 use std::collections::{HashSet, HashMap};
 #[derive(PartialEq)]
-struct Anagram {
-    chars: HashMap<String, u16>,
+struct AnagramMap {
+    chars: HashMap<char, u16>,
 }
 
-impl Anagram {
-    pub fn new(str: &str) -> Self {
-        let mut chars: HashMap<String, u16> = HashMap::new();
+impl AnagramMap {
+    pub fn new(str: String) -> Self {
+        let mut chars: HashMap<char, u16> = HashMap::new();
 
         for c in str.chars() {
-            let lowercase : String = c.to_lowercase().collect();
-
-            let entry = chars.entry(lowercase).or_insert(0);
+            let entry = chars.entry(c).or_insert(0);
             *entry += 1;
         }
 
-        Anagram {chars: chars}
+        AnagramMap {chars: chars}
     }
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a[&str]) -> HashSet<&'a str> {
     let mut result: HashSet<&'a str> = HashSet::new();
 
-    let word_anagram = Anagram::new(word);
+    let word_map = AnagramMap::new(word.to_lowercase());
 
     for possible in possible_anagrams {
         if possible.len() != word.len() || *possible.to_lowercase() == word.to_lowercase() {
             continue;
         }
 
-        let possible_anagram = Anagram::new(possible);
-        if possible_anagram == word_anagram {
+        let possible_map = AnagramMap::new(possible.to_lowercase());
+        if possible_map == word_map {
             result.insert(possible);
         }
     }
