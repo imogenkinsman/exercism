1ab20945a40d6da65c56630e26d520b54c7a2625
Imogen Kinsman <jade@thezets.com>
2020-09-12 13:13:18 -0700

    Space Age: refactor to use a macro

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -25,50 +25,21 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 7600543.81992
-    }
+macro_rules! planet_impl {
+    ($type:ty, $earth_years:expr) => {
+        impl Planet for $type {
+            fn years_during(d: &Duration) -> f64 {
+                d.seconds as f64 / (31557600.0 * $earth_years)
+            }
+        }
+    };
 }
 
-impl Planet for Venus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 19414149.05217
-    }
-}
-
-impl Planet for Earth {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 31557600.0
-    }
-}
-
-impl Planet for Mars {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 59354032.69007
-    }
-}
-
-impl Planet for Jupiter {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 374355659.124
-    }
-}
-
-impl Planet for Saturn {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 929292362.8848
-    }
-}
-
-impl Planet for Uranus {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 2651370019.3296
-    }
-}
-
-impl Planet for Neptune {
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 5200418560.03200
-    }
-}
+planet_impl!(Mercury, 0.2408467);
+planet_impl!(Venus, 0.61519726);
+planet_impl!(Earth, 1.0);
+planet_impl!(Mars, 1.8808158);
+planet_impl!(Jupiter, 11.862615);
+planet_impl!(Saturn, 29.447498);
+planet_impl!(Uranus, 84.016846);
+planet_impl!(Neptune, 164.79132);
